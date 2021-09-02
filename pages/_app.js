b6f80c25102ad545c9be694379774241607a8c50
Finsch <ichbinfenris@gmail.com>
2021-09-02 13:53:09 +0700

    fixed layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/bootstrap.min.css';
 import '../styles/globals.scss';
-import Layout from '../components/Layout/Layout';
+import Layout from '../components/layout/Layout';
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
 config.autoAddCss = false
