495950a1f06b9ccbeb74c28879e2b22ed2347e40
Finsch <ichbinfenris@gmail.com>
2021-09-03 13:42:05 +0700

    minor fixed font size

--- pages/sign-in/index.js
+++ pages/sign-in/index.js
@@ -1,11 +1,21 @@
+import Head from 'next/head'
 import Link from 'next/link';
 
 function SignIn() {
     return (
+        <div>
+            <Head>
+                <title>SF Brandname - Services</title>
+                <meta name="description" content="มองหาร้านแบรนด์เนมมือสองที่ให้ราคาดี ของแท้ คุณภาพสวย ต้องที่ SF Brandname เท่านั้น เราให้บริการแบบครบวงจร ตั้งแต่ขายสินค้า รับซื้อ และทำสปากระเป๋า" />
+                <meta name="keyword" content="ร้านแบรนด์เนมมือสอง ราคาดี, ร้านรับซื้อขายของแบรนด์เนมมือสอง, ร้านรับซื้อกระเป๋าแบรนด์เนมมือสอง, ร้านรับซื้อฝากขายแบรนด์เนมแท้, ร้านรับซื้อและฝากขายแบรนด์เนม, ร้านรับซื้อและฝากขายแบรนด์เนม มือสอง, ร้านรับซื้อ-ฝากขายกระเป๋าแบรนด์เนม, ร้านฝากขายกระเป๋าแบรนด์เนม, ร้านขายสินค้าแบรนด์เนมมือสอง ให้ราคาสูง, ร้านจำนำกระเป๋าแบรนด์เนม
+"/>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
         <div className="container-fluid">
             <div className="row">
-                <div className="col-4 px-5 py-5 d-flex flex-column justify-content-between">
-                    <h1 className="page-title">Sign In</h1>
+                <div className="col-4 px-5 py-5 d-flex flex-column align-items-start">
+                    <h1>Sign In</h1>
                     <form>
                         <div className="mb-5">
 
@@ -40,6 +50,8 @@ function SignIn() {
                 </div>
             </div>
         </div >
+        </main>
+        </div>
     )
 }
 
