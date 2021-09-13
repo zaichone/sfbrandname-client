7d7c777781de6b80a66d27bc74caee4463c6ebe5
Finsch <ichbinfenris@gmail.com>
2021-09-14 01:26:44 +0700

    add messages

--- pages/orders/[id].js
+++ pages/orders/[id].js
@@ -1,23 +1,67 @@
 import { useRouter } from 'next/router'
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import Head from 'next/head'
 import Link from 'next/link';
 import PagtTitle from '../../components/layout/PageTitle';
 import Heading from '../../components/layout/Heading';
 import cover from '../../assets/certificate/cover.png';
-import {Tabs, Tab, Modal, Row, Button, Col, Form, Card, Container} from "react-bootstrap";
+import { Tabs, Tab, Modal, Row, Button, Col, Form, Card, Container } from "react-bootstrap";
 import TelegramIcon from '@material-ui/icons/Telegram';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
+
+import { auth, firestore } from "../../src/config/firebase";
 
 function OrderDetail() {
     const router = useRouter()
     const { id } = router.query;
+    const [clientId, setClientId] = useState();
+    const [text, setText] = useState('');
     const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState();
+    console.log(clientId);
+    const MesagesRef = firestore.collection('messages');
     const handleTextChange = (e) => {
-        let text = e.target.value;
-        setMessage(text);
+        let msg = e.target.value;
+        setText(msg);
         console.log('text is', text);
+        setMessage({
+            clientId: clientId,
+            taskId: id,
+            message: msg,
+            owner: 'client',
+            timestamp: new Date().getTime()
+        });
+        console.log('message is', message);
     }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log('submiting')
+        await MesagesRef.add(message)
+            .then(function () {
+                console.log("Document successfully written Text!", new Date().toISOString());
+            })
+            .catch(function (error) {
+                console.error("Error writing document: ", error);
+            });
+        setText('');
+
+    }
+    useEffect(() => {
+        const _clientId = window.localStorage.getItem('clientId');
+        setClientId(_clientId);
+        const _mesagesRef = firestore.collection('messages').where('clientId','==',_clientId).where('taskId', '==', id).orderBy('timestamp', 'asc');
+        const unsubscribe = _mesagesRef
+            .onSnapshot((snapshot) => {
+                const data = snapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    ...doc.data(),
+                }));
+                console.log('messages data', data)
+                setMessages(data);
+            });
+        return () => unsubscribe();
+
+    }, []);
     return (
 
         <div>
@@ -35,30 +79,39 @@ function OrderDetail() {
                         <div className="row">
 
                             <div className="col-12 col-sm-12 gx-0">
-                            <Tabs defaultActiveKey="home" 
-                               id="orderDetails">
-                             <Tab eventKey="home" title="Item Info">
-                                 <h3 className="tab-title">General Information</h3>
-                             </Tab>
-                             <Tab eventKey="servives" title="Servives">
-                                 <h1>Servives</h1>
-                             </Tab>
-                             <Tab eventKey="message" title="Message">
-                                 <h3 className="tab-title">Have something in mind ?</h3>
-                                 <p>Chat with us, let’s see anything we can help.</p>
-                                 <div className="chatBox">
-                                 <div className="input-group input-bar">
-                            <input type="text" className="form-control" placeholder="What help do you need from us?" onChange={handleTextChange}/>
-                            <button className="btn" type="button" id="button-addon2"><AttachFileIcon/></button>
-                            <button className="btn" type="button" id="button-addon2"><TelegramIcon/></button>
-                        </div>
-                                 </div>
-                             </Tab>
-                             <Tab eventKey="transaction" title="Transaction">
-                                 <h3 className="tab-title">General Information</h3>
-                             </Tab>
-                         </Tabs>
-                               
+                                <Tabs defaultActiveKey="home"
+                                    id="orderDetails">
+                                    <Tab eventKey="home" title="Item Info">
+                                        <h3 className="tab-title">General Information</h3>
+                                    </Tab>
+                                    <Tab eventKey="servives" title="Servives">
+                                        <h1>Servives</h1>
+                                    </Tab>
+                                    <Tab eventKey="message" title="Message">
+                                        <h3 className="tab-title">Have something in mind ?</h3>
+                                        <p>Chat with us, let’s see anything we can help.</p>
+                                        <form onSubmit={(e) => { handleSubmit(e) }}>
+                                            <div className="chatBox">
+                                                <div className="message-lists">
+                                                    {
+                                                        messages?.map((msg) =>
+                                                        <p key={msg.id} className={msg.owner}>{msg.message}</p>
+                                                        )
+                                                    }
+                                                </div>
+                                                <div className="input-group input-bar">
+                                                    <input type="text" className="form-control" placeholder="What help do you need from us?" value={text} onChange={handleTextChange} />
+                                                    <button className="btn" type="button" id="button-addon2"><AttachFileIcon /></button>
+                                                    <button className="btn" type="submit" id="button-addon2"><TelegramIcon /></button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </Tab>
+                                    <Tab eventKey="transaction" title="Transaction">
+                                        <h3 className="tab-title">General Information</h3>
+                                    </Tab>
+                                </Tabs>
+
                             </div>
                         </div>
                     </div>
