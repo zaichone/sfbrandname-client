6cf1f6d3a379b0460e0af5ce6366e114edd1477f
Thanapat Sanongyat <thanapatsanongyat@gmail.com>
2021-09-26 15:51:19 +0700

    account profile page

--- components/layout/PageTitle.js
+++ components/layout/PageTitle.js
@@ -1,12 +1,14 @@
-function PageTitle({title, bg}) {
-    return (
-        <section className="page-title" style={{ backgroundImage: `url(${bg.src})` }}>
-            <div className="cover">
-            <h1 className="heading">{title}</h1>
-            </div>
-            
-        </section>
-    )
+function PageTitle({ title, bg, className }) {
+  return (
+    <section
+      className={`page-title ${className}`}
+      style={{ backgroundImage: `url(${bg.src})` }}
+    >
+      <div className="cover">
+        <h1 className="heading">{title}</h1>
+      </div>
+    </section>
+  );
 }
 
-export default PageTitle
+export default PageTitle;
