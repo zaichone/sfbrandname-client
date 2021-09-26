69671982471d680f359b44dca38f1dd8c6392490
Thanapat Sanongyat <thanapatsanongyat@gmail.com>
2021-09-26 15:09:43 +0700

    sign in and register

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,55 +1,56 @@
-import Link from 'next/link';
-import Logo from '../../assets/Logo.png';
-import { useRouter } from 'next/router';
+import Link from "next/link";
+import Logo from "../../assets/Logo.png";
+import { useRouter } from "next/router";
 
-import { Nav, Navbar, NavDropdown, Container } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
 
 import { auth } from "../../src/config/firebase";
 
 import useAuth from "../../src/hook/auth";
 
 function Header() {
+  const { user, logout } = useAuth();
+  //console.log("🚀 ~ file: Header.js ~ line 10 ~ Header ~ user", user)
+  const router = useRouter();
 
-    const { user, logout } = useAuth();
-    //console.log("🚀 ~ file: Header.js ~ line 10 ~ Header ~ user", user)
-    const router = useRouter()
-
-    async function signOut() {
-        await logout();
-        //window.location.reload(); 
-        //return router.push("/sign-in/");
-    }
-    return (
-        <header className="top-header">
-
-
-
-            <Navbar expand="lg" bg="dark" variant="dark">
-                <Container className="justify-content-around py-3 py-sm-0">
-                    <Navbar.Brand href="/"><img src={Logo.src} className="img-fluid img-logo" alt="SF Brandname" /></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse className="position-relative" id="basic-navbar-nav">
-                        <Nav className="ms-auto">
-                            <Nav.Link href="/how-it-work/">How it Work</Nav.Link>
-                            <Nav.Link href="/services/">Services</Nav.Link>
-                            <Nav.Link href="/authentication/">Authentication</Nav.Link>
-                            {!user &&
-                                <Nav.Link href="/sign-in/">Sign In</Nav.Link>
-                            }
-                            {user &&
-                                <>
-                                    <Nav.Link href="/orders/">My Order</Nav.Link>
-                                    <Nav.Link href="/account/">My Account</Nav.Link>
-                                    <Nav.Link href="#"><button onClick={signOut}>Sign Out</button></Nav.Link>
-                                </>
-                            }
-
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </header>
-    )
+  async function signOut() {
+    await logout();
+    //window.location.reload();
+    //return router.push("/sign-in/");
+  }
+  return (
+    <header className="top-header">
+      <Navbar expand="lg" bg="dark" variant="dark">
+        <Container className="justify-content-around py-3 py-sm-0">
+          <Navbar.Brand href="/">
+            <img
+              src={Logo.src}
+              className="img-fluid img-logo"
+              alt="SF Brandname"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="position-relative" id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/how-it-work/">How it Work</Nav.Link>
+              <Nav.Link href="/services/">Services</Nav.Link>
+              <Nav.Link href="/authentication/">Authentication</Nav.Link>
+              {!user && <Nav.Link href="/sign-in/">Sign In</Nav.Link>}
+              {user && (
+                <>
+                  <Nav.Link href="/orders/">My Order</Nav.Link>
+                  <Nav.Link href="/account/">My Account</Nav.Link>
+                  <Nav.Link href="#">
+                    <button onClick={signOut}>Sign Out</button>
+                  </Nav.Link>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
--- pages/sign-in/index.js
+++ pages/sign-in/index.js
@@ -1,80 +1,111 @@
-import Head from 'next/head'
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React, { useCallback, useContext, useState } from "react";
 
 import { withPublic } from "../../src/hook/route";
 
 function SignIn({ auth }) {
+  const [loading, setLoading] = useState(false);
+  const { user, loginWithEmailAndPassword, error } = auth;
+  console.log("🚀 ~ file: index.js ~ line 10 ~ SignIn ~ user", user);
+  const router = useRouter();
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { email, password } = event.target.elements;
 
-    const [loading, setLoading] = useState(false)
-    const { user, loginWithEmailAndPassword, error } = auth;
-    console.log("🚀 ~ file: index.js ~ line 10 ~ SignIn ~ user", user)
-    const router = useRouter()
-    async function handleSubmit(event) {
-        event.preventDefault();
-        const { email, password } = event.target.elements;
+    await loginWithEmailAndPassword(email.value, password.value);
+    console.log("logged in with user", user);
 
-        await loginWithEmailAndPassword(email.value, password.value)
-        console.log('logged in with user', user);
-
-        setLoading(false)
-    }
-    /*
+    setLoading(false);
+  }
+  /*
         if (user) {
             return router.push('/');
         } */
-    return (
-        <div>
-            <Head>
-                <title>SF Brandname - Sign In</title>
-                <meta name="description" content="มองหาร้านแบรนด์เนมมือสองที่ให้ราคาดี ของแท้ คุณภาพสวย ต้องที่ SF Brandname เท่านั้น เราให้บริการแบบครบวงจร ตั้งแต่ขายสินค้า รับซื้อ และทำสปากระเป๋า" />
-                <meta name="keyword" content="ร้านแบรนด์เนมมือสอง ราคาดี, ร้านรับซื้อขายของแบรนด์เนมมือสอง, ร้านรับซื้อกระเป๋าแบรนด์เนมมือสอง, ร้านรับซื้อฝากขายแบรนด์เนมแท้, ร้านรับซื้อและฝากขายแบรนด์เนม, ร้านรับซื้อและฝากขายแบรนด์เนม มือสอง, ร้านรับซื้อ-ฝากขายกระเป๋าแบรนด์เนม, ร้านฝากขายกระเป๋าแบรนด์เนม, ร้านขายสินค้าแบรนด์เนมมือสอง ให้ราคาสูง, ร้านจำนำกระเป๋าแบรนด์เนม
-"/>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main>
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-4 px-xl-4 px-xxl-5 py-5 d-flex flex-column justify-content-around">
-                            <h1 className="mt-5">Sign In</h1>
-                            <form onSubmit={handleSubmit} className="w-100 mt-5">
-                                <div className="mb-4">
-
-                                    <input type="email" className="form-control" id="email" name="email" placeholder="Enter your email" />
-                                </div>
-                                <div className="mb-5">
-
-                                    <input type="password" className="form-control " id="password" name="password" placeholder="Password" />
-                                </div>
-                                <div className="mb-5">
-                                    <div className="row">
-                                        <div className="form-check col-6">
-                                            <input className="form-check-input" type="checkbox" value="" id="keepMeSignedIn" />
-                                            <label className="form-check-label" htmlFor="keepMeSignedIn">
-                                                Keep me signed in
-                                            </label>
-                                        </div>
-
-                                        <div className="col-6">
-                                            <p className="forget-password-text text-end"><Link href="/forget-password/">Forget Password</Link></p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="mb-3">
-                                    <button type="submit" className="mb-3">Login</button>
-                                </div>
-                            </form>
-                            <p className="register-text text-center"><Link href="/register/">Don’t have account? Register here.</Link></p>
-                        </div>
-                        <div className="col-8 p-0">
-                            <div className="register-cover"></div>
-                        </div>
+  return (
+    <div>
+      <Head>
+        <title>SF Brandname - Sign In</title>
+        <meta
+          name="description"
+          content="มองหาร้านแบรนด์เนมมือสองที่ให้ราคาดี ของแท้ คุณภาพสวย ต้องที่ SF Brandname เท่านั้น เราให้บริการแบบครบวงจร ตั้งแต่ขายสินค้า รับซื้อ และทำสปากระเป๋า"
+        />
+        <meta
+          name="keyword"
+          content="ร้านแบรนด์เนมมือสอง ราคาดี, ร้านรับซื้อขายของแบรนด์เนมมือสอง, ร้านรับซื้อกระเป๋าแบรนด์เนมมือสอง, ร้านรับซื้อฝากขายแบรนด์เนมแท้, ร้านรับซื้อและฝากขายแบรนด์เนม, ร้านรับซื้อและฝากขายแบรนด์เนม มือสอง, ร้านรับซื้อ-ฝากขายกระเป๋าแบรนด์เนม, ร้านฝากขายกระเป๋าแบรนด์เนม, ร้านขายสินค้าแบรนด์เนมมือสอง ให้ราคาสูง, ร้านจำนำกระเป๋าแบรนด์เนม
+"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12 col-sm-4 px-xl-4 px-xxl-5 py-5 d-flex flex-column justify-content-around">
+              <h1 className="mt-5">Sign In</h1>
+              <form onSubmit={handleSubmit} className="w-100 mt-5">
+                <div className="mb-4">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    name="email"
+                    placeholder="Enter your email"
+                  />
+                </div>
+                <div className="mb-5">
+                  <input
+                    type="password"
+                    className="form-control "
+                    id="password"
+                    name="password"
+                    placeholder="Password"
+                  />
+                </div>
+                <div className="mb-5">
+                  <div className="row">
+                    <div className="form-check col-6 ">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id="keepMeSignedIn"
+                      />
+                      <label
+                        className="form-check-label"
+                        htmlFor="keepMeSignedIn"
+                      >
+                        Keep me signed in
+                      </label>
                     </div>
-                </div >
-            </main>
+
+                    <div className="col-6">
+                      <p className="forget-password-text text-end">
+                        <Link href="/forget-password/">Forget Password</Link>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mb-3">
+                  <button type="submit" className="mb-3">
+                    Login
+                  </button>
+                </div>
+              </form>
+              <p className="register-text text-center">
+                <Link href="/register/">
+                  Don’t have account? Register here.
+                </Link>
+              </p>
+            </div>
+            <div className=" d-none d-sm-inline col-sm-8 p-0">
+              <div className="register-cover"></div>
+            </div>
+          </div>
         </div>
-    )
+      </main>
+    </div>
+  );
 }
 
-export default withPublic(SignIn)
+export default withPublic(SignIn);
