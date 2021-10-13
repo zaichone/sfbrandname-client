1d9f70f6b75a3736af216ae7accb7eee74fd801e
Thanapat Sanongyat <thanapatsanongyat@gmail.com>
2021-10-13 16:57:37 +0700

    Update AuthService.js

--- src/service/AuthService.js
+++ src/service/AuthService.js
@@ -1,68 +1,80 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
+import { firestore } from "../config/firebase";
+
 export const AuthService = {
-	loginWithGoogle: async () => {
-		const provider = new firebase.auth.GoogleAuthProvider();
-		try {
-			const userCred = await firebase.auth().signInWithPopup(provider);
-			return {
-				user: userCred.user,
-			};
-		} catch (e) {
-			return {
-				error: e.message,
-			};
-		}
-	},
-    loginWithEmailAndPassword: async (email, password) => {
-		const provider = new firebase.auth.GoogleAuthProvider();
-		try {
-			const userCred = await firebase.auth().signInWithEmailAndPassword(email, password);
-			return {
-				user: userCred.user,
-			};
-		} catch (e) {
-			return {
-				error: e.message,
-			};
-		}
-	},
-	signUpWithEmailAndPassword: async (email, password, firstName, lastName) => {
-		const provider = new firebase.auth.GoogleAuthProvider();
-		try {
-			const userCred = await firebase.auth().createUserWithEmailAndPassword(email, password);
-			await firestore.collection("members").doc(userCred.user.uid).set({
-                authProvider: "local",
-                email,
-                firstName,
-                lastName,
-                userRole: 'client',
-                createAt: new Date().getTime(),
-				status:'active'
-            });
-            alert('Sing up sucess');
-			return {
-				user: userCred.user,
-			};
-		} catch (e) {
-			return {
-				error: e.message,
-			};
-		}
-	},
-	logout: async () => {
-		await firebase.auth().signOut();
-	},
-	sendPasswordResetEmail: async (email) => {
-		try {
-			const userCred = await firebase.auth().sendPasswordResetEmail(email);
-            console.log("🚀 ~ file: AuthService.js ~ line 60 ~ sendPasswordResetEmail: ~ userCred", userCred)
-			
-		} catch (e) {
-			return {
-				error: e.message,
-			};
-		}
-	},
+  loginWithGoogle: async () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    try {
+      const userCred = await firebase.auth().signInWithPopup(provider);
+      return {
+        user: userCred.user,
+      };
+    } catch (e) {
+      return {
+        error: e.message,
+      };
+    }
+  },
+  loginWithEmailAndPassword: async (email, password) => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    try {
+      const userCred = await firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password);
+      return {
+        user: userCred.user,
+      };
+    } catch (e) {
+      return {
+        error: e.message,
+      };
+    }
+  },
+  signUpWithEmailAndPassword: async (email, password, firstName, lastName) => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    try {
+      const userCred = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password);
+      await firestore
+        .collection("members")
+        .doc(userCred.user.uid)
+        .set({
+          authProvider: "local",
+          email,
+          firstName,
+          lastName,
+          displayName: firstName + " " + lastName,
+          userRole: "client",
+          createAt: new Date().getTime(),
+          status: "active",
+        });
+      alert("Sign up success!");
+      return {
+        user: userCred.user,
+      };
+    } catch (e) {
+      return {
+        error: e.message,
+      };
+    }
+  },
+  logout: async () => {
+    await firebase.auth().signOut();
+  },
+  sendPasswordResetEmail: async (email) => {
+    try {
+      const userCred = await firebase.auth().sendPasswordResetEmail(email);
+      console.log(
+        "🚀 ~ file: AuthService.js ~ line 60 ~ sendPasswordResetEmail: ~ userCred",
+        userCred
+      );
+    } catch (e) {
+      return {
+        error: e.message,
+      };
+    }
+  },
 };
