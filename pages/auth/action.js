6ea67c099cb8b782c938c0e44979506eb83a4a48
Finsch <ichbinfenris@gmail.com>
2021-10-14 11:48:18 +0700

    test function save password

--- pages/auth/action.js
+++ pages/auth/action.js
@@ -22,6 +22,34 @@ function Action() {
 
         //await sendPasswordResetEmail(email.value);
         console.log("reseting password");
+        auth.verifyPasswordResetCode(oobCode).then((email) => {
+            var accountEmail = email;
+            console.log("🚀 ~ file: action.js ~ line 27 ~ auth.verifyPasswordResetCode ~ accountEmail", accountEmail)
+        
+            // TODO: Show the reset screen with the user's email and ask the user for
+            // the new password.
+            var newPassword = confirmPassword.value;
+        
+            // Save the new password.
+            auth.confirmPasswordReset(oobCode, newPassword).then((resp) => {
+              // Password reset has been confirmed and new password updated.
+        
+              // TODO: Display a link back to the app, or sign-in the user directly
+              // if the page belongs to the same domain as the app:
+              // auth.signInWithEmailAndPassword(accountEmail, newPassword);
+        
+              // TODO: If a continue URL is available, display a button which on
+              // click redirects the user back to the app via continueUrl with
+              // additional state determined from that URL's parameters.
+            }).catch((error) => {
+              // Error occurred during confirmation. The code might have expired or the
+              // password is too weak.
+              setError(error)
+            });
+          }).catch((error) => {
+            // Invalid or expired action code. Ask user to try to reset the password
+            // again.
+          });
         setNoti('Your new password is updated')
         setLoading(false);
     }
